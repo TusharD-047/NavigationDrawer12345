beb9bf017219e044725dc80224f1b02677078a85
TusharD-047 <56781715+TusharD-047@users.noreply.github.com>
2020-06-28 11:40:55 +0530

    Signup change

--- app/src/main/java/com/nopalyer/navigationdrawer/Login/SignUp.java
+++ app/src/main/java/com/nopalyer/navigationdrawer/Login/SignUp.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,7 +22,9 @@ public class SignUp extends AppCompatActivity {
 
     private EditText Email,pass;
     private Button go;
+    private TextView showHide;
     private FirebaseAuth firebaseAuth;
+    private int ShowPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,32 @@ public class SignUp extends AppCompatActivity {
 
         Email = findViewById(R.id.crroll);
         pass = findViewById(R.id.crpass);
+        showHide=(TextView)findViewById(R.id.newpass1);
         go = findViewById(R.id.crbuttn);
+
+        ShowPass = 1;
+        showHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ShowPass ==1)
+                {
+                    ShowPass =0;
+                    pass.setTransformationMethod(null);
+                    if(pass.getText().length()>0)
+                        pass.setSelection(pass.getText().length());
+                    showHide.setBackgroundResource(R.drawable.ic_images2);
+
+                }
+                else{
+                    ShowPass=1;
+                    pass.setTransformationMethod(new PasswordTransformationMethod());
+                    if(pass.getText().length()>0)
+                        pass.setSelection(pass.getText().length());
+                    showHide.setBackgroundResource(R.drawable.ic_images1);
+                }
+            }
+        });
+
 
         firebaseAuth = FirebaseAuth.getInstance();
 
