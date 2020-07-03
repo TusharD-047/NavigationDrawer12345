8cfbfe50b6966b70d65106ba69e57ff53780b659
ShivaniSachan <shivanisachan2000@gmail.com>
2020-07-03 15:47:05 +0530

    login activity

---
+++ app/src/main/java/com/nopalyer/navigationdrawer/dual_batch.java
@@ -0,0 +1,14 @@
+package com.nopalyer.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dual_batch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dual_batch);
+    }
+}
---
+++ app/src/main/java/com/nopalyer/navigationdrawer/signup_option.java
@@ -0,0 +1,14 @@
+package com.nopalyer.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signup_option extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_option);
+    }
+}
