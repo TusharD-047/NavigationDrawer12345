6890cbe541ad79beee9c6e85ff83b1ad22f6d5b0
ShivaniSachan <shivanisachan2000@gmail.com>
2020-08-01 14:44:59 +0530

    courses

---
+++ app/src/main/java/com/nopalyer/navigationdrawer/Courses.java
@@ -0,0 +1,14 @@
+package com.nopalyer.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Courses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
